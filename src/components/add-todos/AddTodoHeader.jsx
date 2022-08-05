7b14eea40132cdd1992adeb1c7797d9fca37e16d
ahmad-mrf <naashna20@gmail.com>
2022-08-05 11:35:40 +0430

    add ' add todo ' logic

--- src/components/add-todos/AddTodoHeader.jsx
+++ src/components/add-todos/AddTodoHeader.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import ColorTag from "../ui/ColorTag";
 import styles from "./addTodoHeader.module.css";
-const addTodoHeader = () => {
+const addTodoHeader = (props) => {
+  const ChangeHandler = (todoColor) => {
+    props.onGetTodoColor(todoColor);
+  };
   return (
     <div className={styles["add-todo-header-contaner"]}>
       <h3>add new todo</h3>
-      <ColorTag
-        className={styles["add-todo-header-color"]}
-        id="todoColor"
-        value="#3b9ade"
-        label="todo color"
-      />
+      <ColorTag onChangeHandler={ChangeHandler} className={styles["add-todo-header-color"]} id="todoColor" value="#3b9ade" label="todo color" />
     </div>
   );
 };
