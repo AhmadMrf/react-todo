ee5cfbbed5d2b21beb2819c8c608a92f85a7bd84
ahmad-mrf <naashna20@gmail.com>
2022-08-05 11:45:04 +0430

    add 'add todo' logic

--- src/components/todos/TodosWrapper.jsx
+++ src/components/todos/TodosWrapper.jsx
@@ -11,7 +11,7 @@ import AddTodo from "../add-todos/AddTodo";
 const TodosWrapper = () => {
   const {
     userData: { userName, color },
-    onLogedOut
+    onLogedOut,
   } = useContext(authContext);
   const [newTodo, setNewTodo] = useState(false);
   const [todos, setTodos] = useState({});
@@ -22,16 +22,15 @@ const TodosWrapper = () => {
       content: "this is my first todo",
       modifyDate: "2022-07-20 08:23",
       completed: false,
-      color: "#cd12ab"
+      color: "#cd12ab",
     },
     {
       id: 2,
       title: "second todo",
-      content:
-        "this another todo that has multiple jjjg fffjgjg jjkkjkj oioioiio ttyttyty rtrtrtrt yuyuyuio jh ghghhf vf gkjkmhj hhjhj shhzlines",
+      content: "this another todo that has multiple jjjg fffjgjg jjkkjkj oioioiio ttyttyty rtrtrtrt yuyuyuio jh ghghhf vf gkjkmhj hhjhj shhzlines",
       modifyDate: "2022-06-20 12:01",
       completed: false,
-      color: "#5a45cc"
+      color: "#5a45cc",
     },
     {
       id: 3,
@@ -39,8 +38,8 @@ const TodosWrapper = () => {
       content: "lorem lkjds sjdflsjdf sfshef fskfse skfjwe sjfeof d",
       modifyDate: "2022-07-10 14:45",
       completed: true,
-      color: "#cd12ab"
-    }
+      color: "#cd12ab",
+    },
   ];
 
   const logOutHandler = () => {
@@ -61,16 +60,16 @@ const TodosWrapper = () => {
       icon: "info",
       timer: 2500,
       showConfirmButton: false,
-      iconColor: color
+      iconColor: color,
     });
   };
   const editIconStyles = {
-    style: { verticalAlign: "middle", marginLeft: ".5rem", cursor: "pointer" }
+    style: { verticalAlign: "middle", marginLeft: ".5rem", cursor: "pointer" },
   };
   return newTodo ? (
     <AddTodo color={color} />
   ) : (
-    <section>
+    <section style={{ position: "relative" }}>
       <IconContext.Provider value={editIconStyles}>
         <HeaderApp style={{ "--main-color": color }} className="font">
           Hi , {userName}
@@ -80,6 +79,7 @@ const TodosWrapper = () => {
         </HeaderApp>
       </IconContext.Provider>
       <TodosList todos={todosData} />
+      <AddTodo />
     </section>
   );
 };
