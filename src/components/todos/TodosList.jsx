52ecd79e5ef942bafc4cd8a189c51d19aa0e47fb
ahmad-mrf <naashna20@gmail.com>
2022-08-12 11:07:08 +0430

    adding 'no todo' condition

--- src/components/todos/TodosList.jsx
+++ src/components/todos/TodosList.jsx
@@ -2,13 +2,8 @@ import React from "react";
 import Todo from "./Todo";
 import styles from "./todolist.module.css";
 const TodosList = (props) => {
-  return (
-    <ul className={styles.todoslist}>
-      {props.todos.map((todo) => (
-        <Todo key={todo.id} todo={todo} />
-      ))}
-    </ul>
-  );
+  const todos = props.todos.length ? props.todos.map((todo) => <Todo key={todo.id} todo={todo} />) : <p className={styles["no-todo"]}>no todos to show</p>;
+  return <ul className={styles.todoslist}>{todos}</ul>;
 };
 
 export default TodosList;
