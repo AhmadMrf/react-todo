53b5f63e1d8d13f296f46b7f07a526cc48ee4dd0
ahmad-mrf <naashna20@gmail.com>
2022-08-24 11:02:45 +0430

    add 'complete and delete todo function'

---
+++ src/components/todos/MoreActionBtn.jsx
@@ -0,0 +1,80 @@
+import { useState, useRef, useEffect, useContext } from "react";
+import { MdDone, MdOutlineModeEdit, MdOutlineCancel } from "react-icons/md";
+import { BsTrash, BsThreeDots } from "react-icons/bs";
+import styles from "./MoreActionBtn.module.css";
+import authContext from "../../context/authContext";
+import AddTodo from "../add-todos/AddTodo";
+const MoreActionBtn = ({ todoId, onTodoEditHandler }) => {
+  const { onEditUser, userData } = useContext(authContext);
+  console.log(userData);
+  const [showMoreAction, setShowMoreAction] = useState(false);
+  const moreBtns = useRef(null);
+  const { current } = moreBtns;
+  useEffect(() => {
+    const outSideClickHandler = (e) => {
+      if (current && !current.contains(e.target)) {
+        setShowMoreAction(false);
+      }
+    };
+    document.addEventListener("click", outSideClickHandler);
+    return () => {
+      document.removeEventListener("click", outSideClickHandler);
+    };
+  }, [current]);
+
+  const toggleMoreAcrion = (e) => {
+    setShowMoreAction((prev) => !prev);
+  };
+  const completHandler = () => {
+    const newTodosStatus = userData.userTodo.map((todo) => {
+      if (todoId === todo.id) {
+        return { ...todo, completed: true };
+      }
+      return todo;
+    });
+    const newUserData = {
+      ...userData,
+      userTodo: newTodosStatus,
+    };
+    onEditUser(newUserData);
+    setShowMoreAction(false);
+  };
+  const deleteHandler = () => {
+    const newTodos = userData.userTodo.filter((todo) => {
+      return todoId !== todo.id;
+    });
+    const newUserData = {
+      ...userData,
+      userTodo: newTodos,
+    };
+    onEditUser(newUserData);
+    setShowMoreAction(false);
+  };
+  const editHandler = () => {
+    onTodoEditHandler(todoId)
+  };
+  return (
+    <div ref={moreBtns} className={styles["more-action-wrapper"]}>
+      {showMoreAction && (
+        <div className={`${styles["more-action"]}`}>
+          <button onClick={completHandler} className={styles["completed-btn"]}>
+            <MdDone />
+          </button>
+          <button onClick={deleteHandler} className={styles["delete-btn"]}>
+            <BsTrash />
+          </button>
+          <button onClick={editHandler} className={styles["edit-btn"]}>
+            <MdOutlineModeEdit />
+          </button>
+        </div>
+      )}
+      {
+        <button onClick={toggleMoreAcrion} className={styles["toggle-btn"]}>
+          {showMoreAction ? <MdOutlineCancel /> : <BsThreeDots />}
+        </button>
+      }
+    </div>
+  );
+};
+
+export default MoreActionBtn;
--- src/components/todos/Todo.jsx
+++ src/components/todos/Todo.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import TodoDate from "./TodoDate";
 import styles from "./todo.module.css";
+import MoreActionBtn from "./MoreActionBtn";
 const Todo = ({ todo }) => {
+  let todoWrapperStyle = todo.completed ? `${styles["todo-wrapper"]} ${styles["completed"]}` : `${styles["todo-wrapper"]} `;
+  // console.log(styles['todo-title']);
+
   return (
-    <li
-      style={{ "--todo-color": todo.color }}
-      className={styles["todo-wrapper"]}
-    >
+    <li style={{ "--todo-color": todo.color }} className={todoWrapperStyle}>
       <div className={styles["todo-header"]}>
         <div className={styles["todo-details"]}>
           <TodoDate date={todo["modifyDate"]} />
           <span className={styles["todo-title"]}>{todo.title}</span>
         </div>
-        <button className={styles["todo-more-action"]}></button>
+        <MoreActionBtn todoId={todo.id} />
+        {/* <button className={styles["todo-more-action"]}></button> */}
       </div>
       <div className={styles["todo-content"]}>{todo.content}</div>
     </li>
