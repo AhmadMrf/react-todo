7089d83653469f303619ffd27a6d837776467169
ahmad-mrf <naashna20@gmail.com>
2022-09-14 01:54:51 +0430

    add 'delete user logic'

--- src/components/todos/EditUser.jsx
+++ src/components/todos/EditUser.jsx
@@ -6,6 +6,9 @@ import styles from "./editUser.module.css";
 import { useContext } from "react";
 import authContext from "../../context/auth-context";
 
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
 const EditUser = (props) => {
   const [editedUser, setEditedUser] = useState(props.prevUser);
   const [userColor, setUserColor] = useState(props.prevColor);
@@ -22,6 +25,24 @@ const EditUser = (props) => {
     }
   };
 
+  const confirmDeleteUser = () => {
+    const Alert = withReactContent(Swal);
+
+    Alert.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const newEditedUser = { ...userData, doDelete: true };
+        onEditUser(newEditedUser);
+      }
+    });
+  };
   const confirmEditUser = () => {
     if (!validation(editedUser)) {
       setIsValid(() => ({
@@ -30,7 +51,7 @@ const EditUser = (props) => {
       }));
       return;
     }
-    const newEditedUser = { ...userData, userInfo: { userName: editedUser, color: userColor }, doDelete: true };
+    const newEditedUser = { ...userData, userInfo: { userName: editedUser, color: userColor } };
     onEditUser(newEditedUser);
     props.onEditing();
   };
@@ -62,9 +83,14 @@ const EditUser = (props) => {
         />
         <ColorTag id='editColor' value={userColor} onChangeHandler={getUserColor} />
       </div>
-      <div className={styles.buttoms}>
-        <MdDone title='confirm changes' onClick={confirmEditUser} />
-        <MdOutlineCancel title='cancel6' onClick={closeModal} />
+      <div className={styles.buttons}>
+        <div>
+          <MdDone title='confirm changes' onClick={confirmEditUser} />
+          <MdOutlineCancel title='cancel' onClick={closeModal} />
+        </div>
+        <button onClick={confirmDeleteUser} className={styles["delete-button"]}>
+          delete user
+        </button>
       </div>
     </div>
   );
